597fe761ed91bcef88bc8e4bea0b5f7471636225
JanPanjan <89231282@student.upr.si>
2025-03-08 15:38:05 +0100

    plot digits

--- src/gan.py
+++ src/gan.py
@@ -8,6 +8,7 @@ from tensorflow.keras import layers
 import glob
 import imageio
 import os
+import sys
 import PIL
 import time
 
@@ -24,6 +25,9 @@ from IPython import display
 train_imgs = train_imgs.reshape(train_imgs.shape[0], 28, 28, 1)
 train_imgs = train_imgs.astype("float32") / 255.0
 
+test_imgs = test_imgs.reshape(test_imgs.shape[0], 28, 28, 1)
+test_imgs = test_imgs.astype("float32") / 255.0
+
 # create a tf dataset, make batches and shuffle elements
 # smaller batches hinder training potential, but are less memory intensive
 # from docs:
@@ -33,6 +37,7 @@ train_imgs = train_imgs.astype("float32") / 255.0
 #   buffer_size equal to the number of elements in the dataset, resulting in uniform shuffle.
 BATCH_SIZE = 128
 BUFFER_SIZE = train_imgs.shape[0]
+channels = 1
 
 train_df = tf.data.Dataset.from_tensor_slices(train_imgs)
 train_df = train_df.batch(BATCH_SIZE).shuffle(BUFFER_SIZE)
@@ -40,12 +45,22 @@ train_df = train_df.batch(BATCH_SIZE).shuffle(BUFFER_SIZE)
 def build_generator():
     model = tf.keras.Sequential()
 
+    # start with a random vector of size 100, that represents noise
+    model.add(layers.Input(shape=(100,)))
     # x*y*z: x*y is img size while z is number of channels
     # more channels == more depth, but since we have grayscale images, I think
     # 1 channel will be enough for this model
-    model.add(layers.Dense(units=7*7*1, input_shape=(100,)))
+    model.add(layers.Dense(units=7*7*channels))
+
+    return model
+
+
+def build_discriminator():
+    model = tf.keras.Sequential()
 
     return model
 
 
 generator = build_generator()
+print(generator.summary())
+discriminator = build_discriminator()
